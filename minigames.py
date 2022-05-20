105c61106b0de1a3b35028114353cbb4134d7cb1
mpbstrd <matthewperrybustarde@gmail.com>
2022-05-20 14:59:07 +0800

    added small changes on minigames

--- minigames.py
+++ minigames.py
@@ -4,7 +4,7 @@ def rps():
     rps = ['rock','paper','scissors']
 
     bot_choice = random.choice(rps)
-    player_choice = input("Pick: ")
+    player_choice = input("\n Pick: ")
     if player_choice == bot_choice:
         print('You win :D \n You picked: {} \n Bot picked: {}'.format(player_choice,bot_choice))
     else:
@@ -13,18 +13,30 @@ def rps():
 def guessing_game():
     random_number = random.randint(1,100)
     print(random_number)
-    player_pick = int(input("Pick a number: "))
-    for attempts in range(5):
+    
+    attempts = 1
+    while attempts <= 5:
+        player_pick = int(input("Pick a number: "))
         if random_number == player_pick:
-            print("You got the right number in: {} attempt(s)".format(int(attempts+1)))
+            print("You got the right number in: {} attempt(s)".format(int(attempts)))
             break
         elif random_number > player_pick:
             print("The number you are looking for is greater than {}".format(player_pick))
-            player_pick = int(input("Try again: "))
         elif random_number < player_pick:
             print("The number you are looking for is less than {}".format(player_pick))
-            player_pick = int(input("Try again: "))
         else:
             print("Please enter a number between 1-100 only")
-            player_pick = int(input("Try again: "))
+        if attempts == 5:
+                print("You already reached the maximum guesses. Better luck next time!")
+                break
+        attempts += 1
 
+print("Enter 1 for rock, paper and scissors.")
+print("Enter 2 for guessing game.")
+player = int(input("What game shall we play? "))
+if player == 1:
+    rps()
+elif player == 2:
+    guessing_game()
+else:
+    print("Please choose a valid number.")
